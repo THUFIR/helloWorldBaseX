e7b6ccd9067e491e418937a582fe829f0fada5a5
thufir <hawat.thufir@gmail.com>
2019-01-01 07:10:21 -0800

    incremental

--- src/main/java/org/basex/examples/local/App.java
+++ src/main/java/org/basex/examples/local/App.java
@@ -1,83 +1,40 @@
 package org.basex.examples.local;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Properties;
 import java.util.logging.Logger;
 import org.basex.core.BaseXException;
 import org.basex.core.Context;
-import org.basex.core.cmd.Close;
 import org.basex.core.cmd.CreateDB;
-import org.basex.core.cmd.CreateIndex;
 import org.basex.core.cmd.DropDB;
-import org.basex.core.cmd.DropIndex;
-import org.basex.core.cmd.InfoDB;
 import org.basex.core.cmd.List;
-import org.basex.core.cmd.Open;
 
 public final class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
+    private final Context context = new Context();
 
     public static void main(String[] args) throws BaseXException, IOException {
         LOG.fine("starting..");
-        java.util.List<String> stringList = new ArrayList<>();
-        String string = "list";
-        stringList.add(string);
-        new App().foo(stringList);
+        new App().helloWorld();
     }
 
-    private void foo(java.util.List<String> StringList) throws BaseXException, IOException {
-        Properties properties = new Properties();
-        properties.loadFromXML(App.class.getResourceAsStream("/selenium.xml"));
-        LOG.info(properties.toString());
+    private void list() throws BaseXException {
+        LOG.info(new List().execute(context));
+    }
 
-        // Database context.
-        Context context = new Context();
+    private void helloWorld() throws BaseXException, IOException {
+        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
+        String databaseName = properties.getProperty("databaseName");
+        String databasePath = properties.getProperty("databasePath");
 
-        System.out.println("=== RunCommands ===");
+        list();
+        new CreateDB(databaseName,databasePath).execute(context);
+        list();
+        new DropDB(databaseName).execute(context);
+        list();
 
-        // Create a database from a local or remote XML document or XML String
-        System.out.println("\n* Create a database.");
-
-        new CreateDB("DBExample", "/input.xml").execute(context);
-
-        // Close and reopen the database
-        System.out.println("\n* Close and reopen database.");
-
-        new Close().execute(context);
-        new Open("DBExample").execute(context);
-
-        // Additionally create a full-text index
-        System.out.println("\n* Create a full-text index.");
-
-        new CreateIndex("fulltext").execute(context);
-
-        // Show information on the currently opened database
-        System.out.println("\n* Show database information:");
-
-        System.out.print(new InfoDB().execute(context));
-
-        // Drop indexes to save disk space
-        System.out.println("\n* Drop indexes.");
-
-        new DropIndex("text").execute(context);
-        new DropIndex("attribute").execute(context);
-        new DropIndex("fulltext").execute(context);
-
-        // Drop the database
-        System.out.println("\n* Drop the database.");
-
-        new DropDB("DBExample").execute(context);
-
-        // Show all existing databases
-        System.out.println("\n* Show existing databases:");
-
-        System.out.print(new List().execute(context));
-
-        // Close the database context
-        context.close();
     }
 
 }
