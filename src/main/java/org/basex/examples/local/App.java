7eb18d346e34a87ec148b622b74aa9607292cefd
thufir <hawat.thufir@gmail.com>
2019-01-01 17:41:58 -0800

    incremental

--- src/main/java/org/basex/examples/local/App.java
+++ src/main/java/org/basex/examples/local/App.java
@@ -4,46 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.basex.core.BaseXException;
-import org.basex.core.Context;
-import org.basex.core.cmd.CreateDB;
-import org.basex.core.cmd.DropDB;
-import org.basex.core.cmd.List;
 
 public final class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
-    private final Context context = new Context();
-    private URL url = null;
 
     public static void main(String[] args) throws IOException   {
         LOG.fine("starting..");
+
         new App().helloWorld();
     }
 
-    private void list() throws BaseXException {
-        LOG.info(new List().execute(context));
-    }
+    private String fetch() throws MalformedURLException, IOException   {
+        LOG.info(properties.toString());
 
-    private void creatDrop() throws BaseXException {
-        String databaseName = properties.getProperty("databaseName");
-        String databasePath = properties.getProperty("databasePath");
-        list();
-        new CreateDB(databaseName, databasePath).execute(context);
-        list();
-        new DropDB(databaseName).execute(context);
-        list();
-    }
-
-    private void helloWorld() throws IOException {
-        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
-        url = new URL(properties.getProperty("url"));
+        URL url = new URL(properties.getProperty("url"));
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        String html = null;
 
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader bufferedReader = new BufferedReader(
@@ -53,6 +37,15 @@ public final class App {
                 stringBuilder.append(System.getProperty("line.separator"));
             }
         }
-        LOG.info(stringBuilder.toString());
+        html = stringBuilder.toString();
+        LOG.fine(html);
+        return html;
+    }
+
+    private void helloWorld() throws IOException   {
+        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
+        LOG.info(properties.toString());
+        Database database = new Database(properties);
+        database.CreateFromScratch("foo");
     }
 }
---
+++ src/main/java/org/basex/examples/local/Database.java
@@ -0,0 +1,68 @@
+package org.basex.examples.local;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.DropDB;
+import org.basex.core.cmd.List;
+
+public class Database {
+
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
+    private Properties properties = new Properties();
+    private final Context context = new Context();
+
+    private Database() {
+    }
+
+    public Database(Properties properties) {
+        this.properties = properties;
+        LOG.info(properties.toString());
+    }
+
+    private void list() {
+        try {
+            LOG.info(new List().execute(context));
+        } catch (BaseXException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.FINE, null, ex);
+            LOG.info("no databases");
+        }
+    }
+
+    public void CreateFromScratch(String html) {
+        String databaseName = properties.getProperty("databaseName");
+        String databasePath = properties.getProperty("databasePath");
+        LOG.info(databaseName);
+        LOG.info(databasePath);
+
+        list();
+        try {
+            new DropDB(databaseName).execute(context);
+        } catch (BaseXException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            LOG.info("no databases to drop");
+        }
+        list();
+        try {
+            // new CreateDB("DBExample", "src/main/resources/xml/input.xml").execute(context);
+            new CreateDB(databaseName, databasePath).execute(context);
+        } catch (BaseXException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.FINE, null, ex);
+            LOG.severe("cannot create database");
+        }
+        list();
+    }
+
+    private void createDrop() throws BaseXException {
+        String databaseName = properties.getProperty("databaseName");
+        String databasePath = properties.getProperty("databasePath");
+        list();
+        new CreateDB(databaseName, databasePath).execute(context);
+        list();
+        new DropDB(databaseName).execute(context);
+        list();
+    }
+}
