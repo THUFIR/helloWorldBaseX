984b6e6a4d62bb4b1c372caf9d4d79cb66647f91
thufir <hawat.thufir@gmail.com>
2019-01-01 17:03:45 -0800

    incremental

--- src/main/java/org/basex/examples/local/App.java
+++ src/main/java/org/basex/examples/local/App.java
@@ -1,13 +1,14 @@
 package org.basex.examples.local;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
 import java.util.logging.Logger;
 import org.basex.core.BaseXException;
 import org.basex.core.Context;
-//import  org.basex.core.Text.URL;
 import org.basex.core.cmd.CreateDB;
 import org.basex.core.cmd.DropDB;
 import org.basex.core.cmd.List;
@@ -17,7 +18,6 @@ public final class App {
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
     private final Context context = new Context();
-    private String stringUrl = "http://books.toscrape.com/";
     private URL url = null;
 
     public static void main(String[] args) throws IOException {
@@ -25,23 +25,33 @@ public final class App {
         new App().helloWorld();
     }
 
-    private void list() throws BaseXException  {
+    private void list() throws BaseXException {
         LOG.info(new List().execute(context));
     }
 
-    private void helloWorld() throws BaseXException, IOException   {
-        url = new URL(stringUrl);
-        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
+    private void creatDrop() throws BaseXException {
         String databaseName = properties.getProperty("databaseName");
         String databasePath = properties.getProperty("databasePath");
-
         list();
         new CreateDB(databaseName, databasePath).execute(context);
         list();
         new DropDB(databaseName).execute(context);
         list();
-        
-        
-         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    }   
+    }
+
+    private void helloWorld() throws BaseXException, IOException {
+        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
+        url = new URL(properties.getProperty("url"));
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        int code = conn.getResponseCode();
+        LOG.info("connection code\t\t" + code);
+
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), "UTF-8"))) {
+            for (String line; (line = br.readLine()) != null;) {
+                System.out.println(line);
+            }
+        }
+    }
 }
