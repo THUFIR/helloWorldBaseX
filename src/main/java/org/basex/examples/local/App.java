b1fe76a8ab8c7684170e3c1a84ca7dc0ed4d0d0b
thufir <hawat.thufir@gmail.com>
2019-01-02 19:15:17 -0800

    incremental

--- src/main/java/org/basex/examples/local/App.java
+++ src/main/java/org/basex/examples/local/App.java
@@ -17,9 +17,9 @@ public final class App {
     private void helloWorld() throws IOException {
         properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
         LOG.fine(properties.toString());
-        Scraper s = new Scraper(properties);
-        s.init();
-        s.fetch();
+        DatabaseQuery databaseQuery = new DatabaseQuery(properties);
+        databaseQuery.init();
+        databaseQuery.query();
     }
     
 }
---
+++ src/main/java/org/basex/examples/local/DatabaseQuery.java
@@ -0,0 +1,46 @@
+package org.basex.examples.local;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.List;
+import org.basex.core.cmd.Open;
+import org.basex.core.cmd.XQuery;
+
+public class DatabaseQuery {
+
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
+    private Properties properties = new Properties();
+    private URL url = null;
+    private String databaseName = null;
+    private Context context = null;
+    private String parserType = null;
+
+    private DatabaseQuery() {
+    }
+
+    public DatabaseQuery(Properties properties) {
+        this.properties = properties;
+        LOG.fine(properties.toString());
+    }
+
+    public void init() throws MalformedURLException {
+        parserType = properties.getProperty("parserType");
+        url = new URL(properties.getProperty(parserType + "URL"));
+        databaseName = properties.getProperty("databaseName");
+        context = new Context();
+    }
+
+    public void query() throws BaseXException {
+        new Open(databaseName).execute(context);
+        LOG.info(new List().execute(context));
+        LOG.info(new XQuery("//note/body/text()").execute(context));
+        context.close();
+
+    }
+
+}
