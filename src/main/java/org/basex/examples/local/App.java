9bbcdcdfed500d912e21fa119e604cc60a49e3b9
thufir <hawat.thufir@gmail.com>
2019-01-02 19:35:26 -0800

    incremental

--- src/main/java/org/basex/examples/local/App.java
+++ src/main/java/org/basex/examples/local/App.java
@@ -1,25 +1,41 @@
 package org.basex.examples.local;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.basex.core.BaseXException;
 
 public final class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getName());
     private final Properties properties = new Properties();
+    private final String queryXML = "//note/body/text()";
+    private final String queryHTML = "//text()";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws BaseXException, MalformedURLException, IOException {
         LOG.fine("starting..");
         new App().helloWorld();
     }
 
-    private void helloWorld() throws IOException {
-        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
-        LOG.fine(properties.toString());
+    private void queryOnly() throws BaseXException, MalformedURLException {
         DatabaseQuery databaseQuery = new DatabaseQuery(properties);
         databaseQuery.init();
-        databaseQuery.runQuery("//note/body/text()");
+        databaseQuery.runQuery(queryHTML); //ouch
     }
-    
+
+    private void populate() throws MalformedURLException {
+        Scraper scraper = new Scraper(properties);
+        scraper.init();
+        scraper.fetch();
+    }
+
+    private void helloWorld() throws BaseXException, MalformedURLException, IOException {
+        properties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
+        LOG.fine(properties.toString());
+        populate();
+        queryOnly();
+    }
+
 }
